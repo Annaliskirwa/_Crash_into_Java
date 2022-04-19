af0ff139a56c08c6c68098b2b08a1cf906c91602
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-19 21:36:50 +0300

    program to display the resulting grid of a Tic-Tac-Toe game.

---
+++ tictactoegrid.java
@@ -0,0 +1,10 @@
+// You need to write a program to display the resulting grid of a Tic-Tac-Toe game.
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        System.out.println("O X X");
+        System.out.println("O X O");
+        System.out.println("X O X");
+    }
+}
