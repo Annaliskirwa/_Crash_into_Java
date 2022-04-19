023b8d380d38413af6ef21d1ddcce338253f4a8b
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-19 21:44:23 +0300

    Program to print numbers using for loop

---
+++ loopto4.java
@@ -0,0 +1,11 @@
+// Write a program that prints numbers from 0 to 4 inclusive. Each number must be on a new line.
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        for(int i=0; i<=4; i++) 
+		{
+			System.out.println(i);
+		}
+    }
+}
