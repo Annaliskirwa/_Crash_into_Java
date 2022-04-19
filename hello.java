e9bbff86c98453bfdc41a85831e03e3dad0f68f8
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-19 20:29:31 +0300

    Decoding the java program

--- hello.java
+++ hello.java
@@ -1,5 +1,10 @@
-class Main{
+public class Main{
+    // Baic unit of the program, contains the class keyword followed by the class name.
     public static void main(String[]args){
+        // main nmethod:entry point for the java program
+        // makes the program runnable
+        //String[]args rep sequence of arguments passed to the program from the outside
         System.out.println("Hello");
+        // Displays the string followed by a new line on the screen
     }
 }
