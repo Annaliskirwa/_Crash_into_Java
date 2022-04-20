1093b4b1a247c0b9b1eb6b580e068c1e4e1594a5
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-20 21:06:14 +0300

    Binary arithmetic operations

---
+++ arithmetic.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[]args){
+        // binary operations
+        System.out.println(10 % 3)
+        System.out.println(10 + 3)
+        System.out.println(10 - 3)
+        System.out.println(10 / 3)
+    }
+}
