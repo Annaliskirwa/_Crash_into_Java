d3b7ec419eb5a7132f54f0db9d79060bc7ce8c88
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-20 10:33:35 +0300

    Scanning input using nextLine method

--- scanninginput.java
+++ scanninginput.java
@@ -4,7 +4,8 @@ public class Main{
     public static void main(String[]args){
         Scanner scanner = new Scanner(System.in);
 
-        String name = scanner.next();
+        String name = scanner.next(); //reads input until a whitespace
+        String name1 = scanner.nextLine(); //reads input until the end of the line
 
         System.out.println("Hello" + name + "again");
     }
