9a5a0190bbc723f72ce65cd0462468240834813e
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-20 09:19:19 +0300

    Types in Java

---
+++ typesandvariables.java
@@ -0,0 +1,11 @@
+class Main{
+    public static void main(String[]args){
+        String language = "java";
+        // Assigning a variable of type string to word java
+        int cows = 5;
+        cows = 11; //it will run
+        // Assigning a variable of type integer to cows
+        System.out.println(cows);
+        // Printing the variable stored in cows
+    }
+}
