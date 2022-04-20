8ff7184d11855c7c0528f01970ce462f26325d01
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-20 09:24:41 +0300

    End of line comment

---
+++ comments.java
@@ -0,0 +1,8 @@
+class Program {
+    public static void main(String[] args) {
+        // The line below will be ignored
+        // System.out.println("Hello, World");
+        // It prints the string "Hello, Java"
+        System.out.println("Hello, Java"); // Here can be any comment
+    }
+}
