f2ef6a33f6e2537a2cf62d13553b22509a03b437
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-20 09:25:08 +0300

    Multiline comments

--- comments.java
+++ comments.java
@@ -3,6 +3,10 @@ class Program {
         // The line below will be ignored
         // System.out.println("Hello, World");
         // It prints the string "Hello, Java"
+
+        /* This is a single-line comment */
+        /*  This is an example of
+            a multi-line comment */
         System.out.println("Hello, Java"); // Here can be any comment
     }
 }
