2ad7750421c019799c04a65563137879a5b57ea7
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-24 11:46:16 +0300

    Adding symbols

--- characters.java
+++ characters.java
@@ -2,6 +2,8 @@ class Characters{
     public static void main(String[]args){
         char ch = 'A';
         ch += 10;
+        char c = '1' + '3';
+        System.out.println(c) //prints d
         System.out.println(ch);   // 'K'
         System.out.println(++ch); // 'L'
         System.out.println(++ch); // 'M'
