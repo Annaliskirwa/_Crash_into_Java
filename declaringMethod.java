b9468d6bbb176b786d6b14f563332d317b74a8c2
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-24 13:00:10 +0300

    A method with a return type

--- declaringMethod.java
+++ declaringMethod.java
@@ -8,3 +8,9 @@ public static void isPositive(int num) {
 
 // methods with void return nothing, here used return to exit the method with condition.
 //trying to return a value from a method with a void return type throws a compiler error.
+
+
+// An example of a method with a return type:
+public static int countSeeds(int pWeight, int pAge){
+    return pWeight/5 + pAge;
+}
