b19c9b81b70797f1c685f2532a8eef4f35f95642
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-24 12:18:31 +0300

    Calling an instance method

--- callingMethod.java
+++ callingMethod.java
@@ -2,5 +2,10 @@ class Main{
     public static void main(String[]args){
         double weight = 63.85;
         weight = Math.round(weight);
+
+        // Calling  an instance method
+
+        String name = new String("Annalis"); // created an instance (1)
+        name = name.toLowerCase(); // annalis (2)
     }
 }
