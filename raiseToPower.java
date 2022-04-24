2db698fdde66b6759858949e2441c34cc6a649dc
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-24 19:35:44 +0300

    Raise to power using predifined method

---
+++ raiseToPower.java
@@ -0,0 +1,18 @@
+// You're given the method power that takes two int numbers n and m. The method should return the value of nm as a long value.
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static long power(int n, int m) {
+        // write your code here
+        return (long) Math.pow(n,m);
+    }
+
+    public static void main(String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int n = scanner.nextInt();
+        final int m = scanner.nextInt();
+        System.out.println(power(n, m));
+    }
+}
