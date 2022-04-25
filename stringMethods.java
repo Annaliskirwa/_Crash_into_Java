9a387393035e9da56200670a458e14f9350cdd34
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-25 20:07:57 +0300

    String methods

---
+++ stringMethods.java
@@ -0,0 +1,19 @@
+public static void main(String[]args){
+    String text = "The simple text string";
+
+    boolean empty = text.isEmpty(); // false
+
+    String textInUpperCase = text.toUpperCase(); // "THE SIMPLE TEXT STRING"
+
+    boolean startsWith = textInUpperCase.startsWith("THE"); // true
+
+    /* replace all space characters with empty strings */
+    String noSpaces = textInUpperCase.replace(" ", ""); // "THESIMPLETEXTSTRING"
+
+    String textWithWhitespaces = "\t text with whitespaces   !\n  \t";
+
+    String trimmedText = textWithWhitespaces.trim(); // "text with whitespaces   !"
+
+    // substring(beginIndex, endIndex)
+    // endsWith(suffix)
+}
