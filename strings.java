68462e76e540cfd98fce9ad8e7e91fe9df9b1e5a
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-25 19:57:32 +0300

    Creating a string

---
+++ strings.java
@@ -0,0 +1,7 @@
+class Main{
+    public static void main(String[]args){
+        // creating strings
+        String simple = "Annalis is learning Java";
+        System.out.println(simple);
+    }
+}
