852d9367cb7afb8154ec40d3e207990d8c0ce03b
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-25 19:59:16 +0300

    Null strings

--- strings.java
+++ strings.java
@@ -2,6 +2,7 @@ class Main{
     public static void main(String[]args){
         // creating strings
         String simple = "Annalis is learning Java";
+        String nullString = null; //this is null string, trying to access it throws a null pointer exception
         System.out.println(simple);
     }
 }
