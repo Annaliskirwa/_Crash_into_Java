131102b9826509c764d062548424fdfa914363e8
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-25 20:02:10 +0300

    length and charAt java methods

--- strings.java
+++ strings.java
@@ -3,6 +3,12 @@ class Main{
         // creating strings
         String simple = "Annalis is learning Java";
         String nullString = null; //this is null string, trying to access it throws a null pointer exception
+        
+        // getting the length and characters of a string
+        int len = simple.length(); //hii will return the number of characters in the string
+
+        char theFirstChar = s.charAt(0);//and then this will return the character by its index, here its index 0
+
         System.out.println(simple);
     }
 }
