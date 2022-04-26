f77bfdcc9960fc4467155675f1b07a73d344820a
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-26 16:11:19 +0300

    Array instantiation

--- arrayBasics.java
+++ arrayBasics.java
@@ -1,5 +1,6 @@
 class Main{
     public static void main(String[]args){
         int[] array ; //declaring an array
+        int[] numbers = {1,2,3,4}; //instantiating and initializing an array
     }
 }
