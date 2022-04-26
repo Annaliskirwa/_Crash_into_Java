2fb7de03d6556b4b297dc4781eb8c2fd2e35d5c4
Annaliskirwa <annaliskirwa@gmail.com>
2022-04-26 16:12:43 +0300

    Creating an array using the new keyword

--- arrayBasics.java
+++ arrayBasics.java
@@ -2,5 +2,8 @@ class Main{
     public static void main(String[]args){
         int[] array ; //declaring an array
         int[] numbers = {1,2,3,4}; //instantiating and initializing an array
+
+        int[] numberss = new int[n]; 
+        //where n is the lenth of the array; creating an array using the new keyword
     }
 }
