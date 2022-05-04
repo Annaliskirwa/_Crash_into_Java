06a5212f4beb30808994d6c5e8509d0539e7feae
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-04 15:31:18 +0300

    Component annotation

---
+++ SpringAnnotations/Component.java
@@ -0,0 +1,7 @@
+//@Component is an annotation that allows Spring to automatically detect our custom beans. In other words, without having to write any explicit code, Spring will: Scan our application for classes annotated with @Component. Instantiate them and inject any specified dependencies into them.
+
+@Component 
+
+Public class Teachers{
+    //b;lambka bka
+}
