99992be32891fd8db141a1c19e1ae231f268764c
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-04 15:01:49 +0300

    Requets mapping annotation

---
+++ SpringAnnotations/RequestMapping.java
@@ -0,0 +1,16 @@
+//maps HTTP requests
+//used with classes and methods
+//optional elements such as consumes, name,method,request,path
+
+@Controller
+
+public class FlowersController
+{
+    @RequestMapping("/red-color/flowers")
+    public String getAllFlowers(Model model)
+    {
+        //code the klogic here
+    
+    }
+    return "flowerlist"
+}
