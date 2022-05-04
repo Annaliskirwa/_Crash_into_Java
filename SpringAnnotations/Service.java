87d810e596394e7ccd0f0e6e4484c4557576da3e
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-04 09:24:43 +0300

    Service annotation

---
+++ SpringAnnotations/Service.java
@@ -0,0 +1,9 @@
+//It is used at the class level. It shows that the annotated class is a service class
+
+@Service
+public class TestService
+{
+    public void serviceFirst(){
+        //business logic call for apis here
+    }
+}
