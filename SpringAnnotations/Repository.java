d67a88a94700c590c5c0e59df71bee0236c8cb5b
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-04 09:29:06 +0300

    Repository annotation

---
+++ SpringAnnotations/Repository.java
@@ -0,0 +1,8 @@
+//It is a Data Access Object that accesses the database directly
+
+@Repository
+public class TestRepository{
+    public void delete(){
+        //database persistence code
+    }
+}
