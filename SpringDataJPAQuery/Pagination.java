cb3f183aecda1ce98036162f63d65806e02210f7
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-06 10:33:57 +0300

    Pagination in native

--- SpringDataJPAQuery/Pagination.java
+++ SpringDataJPAQuery/Pagination.java
@@ -1,3 +1,11 @@
 //JPQL
 
 @Query(value = "SELECT u FROM User u ORDER BY id") Page<User> findAllUsersWithPagination(Pageable pageable);
+
+//Native Query: declare an additional attribute called countQuery
+
+@Query(
+  value = "SELECT * FROM Users ORDER BY id", 
+  countQuery = "SELECT count(*) FROM Users", 
+  nativeQuery = true)
+Page<User> findAllUsersWithPagination(Pageable pageable);
