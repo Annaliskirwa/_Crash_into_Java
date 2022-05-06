41e413289a3b9b7bf7d23629777f5446ce46084a
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-06 11:36:04 +0300

    insert into db using native query

--- SpringDataJPAQuery/UpdateQueries.java
+++ SpringDataJPAQuery/UpdateQueries.java
@@ -13,3 +13,14 @@ int updateUserSetStatusForName(@Param("status") Integer status,
 @Query(value = "update Users u set u.status = ? where u.name = ?", 
   nativeQuery = true)
 int updateUserSetStatusForNameNative(Integer status, String name);
+
+//insert operation into the database
+//insert is not part of jpa
+//use native query
+@Modifying
+@Query(
+  value = 
+    "insert into Users (name, age, email, status) values (:name, :age, :email, :status)",
+  nativeQuery = true)
+void insertUser(@Param("name") String name, @Param("age") Integer age, 
+  @Param("status") Integer status, @Param("email") String email);
