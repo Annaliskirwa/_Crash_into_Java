370eb2aaa4d22dd2f3870aa7a90e9cf027772973
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-06 10:28:22 +0300

    Sorting by name and length of the name

--- SpringDataJPAQuery/JPQLsort.java
+++ SpringDataJPAQuery/JPQLsort.java
@@ -1,3 +1,10 @@
 //spring data handles the sorting
 
 @Query(value = "SELECT u FROM User u") List<User> findAllUsers(Sort sort);
+
+//sorting by name
+userRepository.findAllUsers(Sort.by("name"));
+
+
+//sorting byu the length of the name
+userRepository.findAllUsers(JpaSort.unsafe("LENGTH(name)"));
