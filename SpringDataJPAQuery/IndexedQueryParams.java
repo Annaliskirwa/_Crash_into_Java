418000d800ba4ff9cd661439db8e2918e84bd8b0
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-06 10:58:08 +0300

    Indexed parameters for native queries

--- SpringDataJPAQuery/IndexedQueryParams.java
+++ SpringDataJPAQuery/IndexedQueryParams.java
@@ -6,3 +6,9 @@ User findUserByStatus(Integer status);
 
 @Query("SELECT u FROM User u WHERE u.status = ?1 and u.name = ?2")
 User findUserByStatusAndName(Integer status, String name);
+
+//indexed parameters in Native
+@Query(
+  value = "SELECT * FROM Users u WHERE u.status = ?1", 
+  nativeQuery = true)
+User findUserByStatusNative(Integer status);
