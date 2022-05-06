fd91404494826ddf23b12ba6e1893e3baeb42f70
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-06 10:52:27 +0300

    indexed parameters in JPQL

---
+++ SpringDataJPAQuery/IndexedQueryParams.java
@@ -0,0 +1,8 @@
+//1. Indexed parameters
+
+//index parameters in JPQL
+@Query("SELECT u FROM User u WHERE u.status = ?1")
+User findUserByStatus(Integer status);
+
+@Query("SELECT u FROM User u WHERE u.status = ?1 and u.name = ?2")
+User findUserByStatusAndName(Integer status, String name);
