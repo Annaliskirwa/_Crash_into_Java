5224508d45bb59dc4bc73aaaa22359fca5782e35
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-08 23:09:29 +0300

    Calling the method with inputs

--- getFirstLastElem.java
+++ getFirstLastElem.java
@@ -8,4 +8,11 @@ public class Main{
             arr[0], arr[arr.length -1]
         };
     }
+
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+        int[] array = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] result = getFirstAndLast(array);
+        Arrays.stream(result).forEach(e -> System.out.print(e + " "));
+    }
 }
