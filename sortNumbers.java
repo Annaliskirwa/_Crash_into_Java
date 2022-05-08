8c1f80211dbef0b7d45b4f2abcead449a9c3403a
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-08 23:36:03 +0300

    Converting the array string to integers to array and calling sort method

--- sortNumbers.java
+++ sortNumbers.java
@@ -11,6 +11,8 @@ public class Main{
     public static void main (String[]args){
         final Scanner scanner = new Scanner(System.in);
         String[] values = scanner.nextLine().split("\\s+");
- 
+        int[] numbers = Arrays.stream(values).mapToInt(Integer::parseInt).toArray();
+        sort(numbers);
+        Arrays.stream(numbers).forEach(e -> System.out.print(e + " "));
     }
 }
