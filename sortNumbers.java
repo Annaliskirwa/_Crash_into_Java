f63e5c218416c45d0eb43a77e7bdb7105be132a9
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-08 23:30:31 +0300

    Method to sort numbers in ascending order

---
+++ sortNumbers.java
@@ -0,0 +1,10 @@
+// Implement a method for sorting a given array of integers in the ascending order.
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main{
+    public static void sort(int[] numbers){
+        Arrays.sort(numbers);
+    }
+}
