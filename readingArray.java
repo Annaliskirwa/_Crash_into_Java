649e1910d79080f3ae530ec7109a14ea3adfcf4c
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-17 15:25:24 +0300

    reading arrays from scanner

---
+++ readingArray.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class readingArray {
+    public static void main(String[]args){
+        try (Scanner scanner = new Scanner(System.in)) {
+            int len = scanner.nextInt();
+
+            int arr[] = new int[len];
+            
+            for (int i = 0; i < len; i++){
+                arr[i] = scanner.nextInt();
+            }
+            System.out.println(Arrays.toString(arr));
+        }
+
+    }
+}
