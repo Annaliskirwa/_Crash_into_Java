cfc2a5ab95795889fcd56a3af709b09e1738ba19
Annaliskirwa <annaliskirwa@gmail.com>
2022-05-22 11:46:42 +0300

    multiples of 3

--- squaresofnumbers.java
+++ squaresofnumbers.java
@@ -2,11 +2,12 @@ import java.util.Arrays;
 
 public class squaresofnumbers {
     public static void main(String[] args) {
-    int[] squareNumbers = new int[10];
-
-    for (int i = 0; i < 10; i++ ){
-        squareNumbers[i] = i * i;
+    int[] squaresofnumbers = new int[10];
+    for (int i = 0; i < 10; i++){
+        squaresofnumbers[i] = i*3;
     }
-    System.out.println(Arrays.toString(squareNumbers));
+    System.out.println(Arrays.toString(squaresofnumbers));
+
+
 }
 }
